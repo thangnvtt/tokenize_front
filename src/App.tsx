63e51b7638c00537cad24ab085d2f35b5add469a
thangnv6 <thangnv@foobla.com>
2022-03-25 06:07:13 +0700

    get depth by socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import { io } from "socket.io-client"
+
 function App() {
   const symbol = 'ETHBTC'
-  const limit = 5
-  const [depth, setDepth] = useState<any>({bids: [], asks: []})
+  const [depth, setDepth] = useState<any>({ bids: [], asks: [] })
   const [amountBid, setAmountBid] = useState<number>(0)
   const [sizeAsk, setSizeAsk] = useState<any>(0)
   const maxAmountBid = 5
   const maxSize = 150
-  const url = `http://localhost:3001/api/depth?symbol=${symbol}&maxAmountBid=${maxAmountBid}&maxSize=${maxSize}`
+  const url = `http://localhost:3001/api/depth?symbol=${symbol}&maxAmountBid=${maxAmountBid}&maxSizeAsk=${maxSize}`
+  const socketUrl = 'ws://localhost:3001'
 
   const bidRender = depth.bids.map((bid, index) =>
     <Row key={index}>
@@ -32,27 +34,37 @@ function App() {
     return depthData.data
   }
 
-  useEffect(()=> {
+  const bindingData = (depth) => {
+    if (depth.bids.length > 0) {
+      const amount = depth.bids.reduce((previous, current) => {
+        const size = Number(current[1])
+        const price = Number(current[0])
+        return previous + size * price
+      }, 0)
+      setAmountBid(amount)
+    }
+
+    if (depth.asks.length > 0) {
+      const size = depth.asks.reduce((previous, current) => {
+        return previous + Number(current[1])
+      }, 0)
+      setSizeAsk(size)
+    }
+    setDepth(depth)
+  }
+
+  useEffect(() => {
+    const socket = io(socketUrl, { transports: ['websocket'] })
+    socket.on("depth_fetch", data => {
+      console.log(data)
+      bindingData(data)
+    })
+
     loadData()
       .then(depth => {
-        if (depth.bids.length > 0) {
-          const amount = depth.bids.reduce((previous, current) => {
-            const size = Number(current[1])
-            const price = Number(current[0])
-            return previous + size * price
-          }, 0)
-          setAmountBid(amount)
-        }
-
-        if (depth.asks.length > 0) {
-          const size = depth.asks.reduce((previous, current) => {
-            return previous + Number(current[1])
-          }, 0) 
-          setSizeAsk(size)
-        }
-        setDepth(depth)
+        bindingData(depth)
       })
-      .catch(error => console.log(error.message))    
+      .catch(error => console.log(error.message))
   }, [])
 
   return (
@@ -60,32 +72,32 @@ function App() {
       <Container>
         <Row>
           <Col className="depth" sm={5}>
-              <Row className="depthHeader">
-                <Col sm={6}>Size</Col>
-                <Col sm={6}>Bid</Col>
-              </Row>
-              {bidRender} 
-              <Row className="summary">
-                <Col>Amount Bid: {amountBid.toFixed(2)}</Col>
-              </Row>
+            <Row className="depthHeader">
+              <Col sm={6}>Size</Col>
+              <Col sm={6}>Bid</Col>
+            </Row>
+            {bidRender}
+            <Row className="summary">
+              <Col>Amount Bid: {amountBid.toFixed(2)}</Col>
+            </Row>
           </Col>
           <Col sm={2}></Col>
           <Col className="depth" sm={5}>
-              <Row className="depthHeader">
-                <Col sm={6}>Ask</Col>
-                <Col sm={6}>Size</Col>
-              </Row>
-              {askRender}
-              <Row></Row>
-              <Row className="summary">
-                <Col>Size Ask: {sizeAsk.toFixed(2)}</Col>
-              </Row>
+            <Row className="depthHeader">
+              <Col sm={6}>Ask</Col>
+              <Col sm={6}>Size</Col>
+            </Row>
+            {askRender}
+            <Row></Row>
+            <Row className="summary">
+              <Col>Size Ask: {sizeAsk.toFixed(2)}</Col>
+            </Row>
           </Col>
         </Row>
-        
+
       </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
